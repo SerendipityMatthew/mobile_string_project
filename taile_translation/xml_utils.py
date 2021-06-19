41e064120e4bdbd8b2e9f155d0b11bca620ca4de
Matthew <xuwanjin1991@Gmail.com>
2021-06-19 16:10:48 +0800

    生成 xml 文件, 并格式化
    Signed-off-by: Matthew <xuwanjin1991@Gmail.com>

---
+++ taile_translation/xml_utils.py
@@ -0,0 +1,53 @@
+import os
+from xml.dom import minidom
+from xml.etree import ElementTree  # 导入ElementTree模块
+
+
+def pretty_xml(element, indent, newline, level=0):  # elemnt为传进来的Elment类，参数indent用于缩进，newline用于换行
+    if element:  # 判断element是否有子元素
+        if (element.text is None) or element.text.isspace():  # 如果element的text没有内容
+            element.text = newline + indent * (level + 1)
+        else:
+            element.text = newline + indent * (level + 1) + element.text.strip() + newline + indent * (level + 1)
+            # else:  # 此处两行如果把注释去掉，Element的text也会另起一行
+            # element.text = newline + indent * (level + 1) + element.text.strip() + newline + indent * level
+    temp = list(element)  # 将element转成list
+    for subelement in temp:
+        if temp.index(subelement) < (len(temp) - 1):  # 如果不是list的最后一个元素，说明下一个行是同级别元素的起始，缩进应一致
+            subelement.tail = newline + indent * (level + 1)
+        else:  # 如果是list的最后一个元素， 说明下一行是母元素的结束，缩进应该少一个
+            subelement.tail = newline + indent * level
+        pretty_xml(subelement, indent, newline, level=level + 1)  # 对子元素进行递归操作
+
+
+def pretty_xml_to_file(source_file, file_path):
+    import xml.dom.minidom
+    dom = xml.dom.minidom.parse(source_file)  # or xml.dom.minidom.parseString(xml_string)
+    pretty_xml_as_string = dom.toprettyxml()
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    path = file_path + os.sep + source_file
+
+    with open(path, "w") as f:
+        f.write(pretty_xml_as_string)
+
+
+def generate_string_res(string_value_dict, file_path: str, file_name: str):
+    from xml.etree.ElementTree import Element, SubElement, ElementTree
+
+    root = Element('resources')
+    # 生成第一个子节点 head]
+    for name_key in string_value_dict:
+        head = SubElement(root, 'string')
+        head.attrib["name"] = name_key
+        head.text = string_value_dict[name_key]
+    tree = ElementTree(root)
+    tree.write(file_name, encoding='utf-8')
+    pretty_xml_to_file(file_name, file_path)
+
+
+def test_xml_generator():
+    name_key_value = {"Matthew": "Mona", "Mona": "1111"}
+    generate_string_res(name_key_value, "pretty_xml_to_file_test.xml")
+
+# test_xml_generator()
